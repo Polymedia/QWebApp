f9b74fd38cbba7d97e219d2e53afd5f9d12f4513
PoolBall <psnopko@gmail.com>
2023-08-29 16:14:02 +0300

    Catch callService exceptions and disconnect

--- QtWebApp/httpserver/httpconnectionhandler.cpp
+++ QtWebApp/httpserver/httpconnectionhandler.cpp
@@ -309,6 +309,7 @@ void HttpConnectionHandler::read()
                 auto fnSendError = [this](const char * msg) {
                     const auto response = QString("HTTP/1.1 500 error on callService \r\nException: %1").arg(msg);
                     socket->write(response.toUtf8().constData());
+                    disconnectFromHost();
                 };
 
                 try {
